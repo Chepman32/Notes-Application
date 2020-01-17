218ae4c5a5947e8ee03a2483960eb7e66da49558
Anton Chepur <antonkerch555@gmail.com>
2020-01-17 19:25:36 +0300

    Добаквлены анимации

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,15 +1,20 @@
 import React, {useContext} from "react";
 import { AlertContext } from "./context/alert/alertContext";
+import { CSSTransition } from "react-transition-group";
 export const Alert =() => {
   const {alert, hide} = useContext(AlertContext)
-    if(!alert.visible) {
-        return null;
-    }
-return <div className={`alert alert-${alert.type || `warning`} alert-dismissible`}>
+return (
+  <CSSTransition in={alert.visible} timeout={{
+    enter: 2000,
+    exit: 2000
+  }} classNames={"alert"} mountOnEnter unmountOnExit>
+ <div className={`alert alert-${alert.type || `warning`} alert-dismissible`}>
   <strong>Внимание!</strong>
   {alert.text}
   <button onClick={hide} type="button" className="close" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
 </div>
+</CSSTransition>
+)
 }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,13 +1,25 @@
-import React from "react";
-const Notes = ({notes, onRemove}) => (
-    <ul className="list-group">
-        {notes.map(note => <li className="list-group-item note" key={note.id}>
+import React, { useContext } from "react";
+import {TransitionGroup, CSSTransition} from "react-transition-group"
+import { AlertContext } from "./context/alert/alertContext";
+const Notes = ({notes, onRemove}) => {
+    const alert = useContext(AlertContext);
+    const removeHandler = note => {
+        onRemove(note.id);
+        alert.show("Заметка была удалена", "danger")
+    }
+    return <TransitionGroup component="ul">
+        <ul className="list-group">
+        {notes.map(note =>
+        <CSSTransition key={note.id} classNames={"note"} timeout={2000}>
+            <li className="list-group-item note">
             <div>
             <strong>{note.title}</strong>
         <small>{note.date}</small>
             </div>
-        <button onClick={() => onRemove(note.id)} type="button" className="btn btn-danger brn-sm">&times;</button>
-        </li>) }
+        <button onClick={() => removeHandler(note)} type="button" className="btn btn-danger brn-sm">&times;</button>
+        </li>
+        </CSSTransition>) }
     </ul>
-)
+    </TransitionGroup>
+}
 export default Notes;
